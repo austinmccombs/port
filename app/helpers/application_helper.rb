d84d2b05b855acc69560eb20bd82b63219ee012b
Austin McCombs <austinmccombs22@gmail.com>
2021-03-30 20:16:27 -0600

    begun implementing the image helper to have placeholders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def image_generator(height:, width:)
+        "http://placehold.it/#{height}x#{width}"
+    end
+
+    def cover_img img, type
+        if img.model.cover_photo? || img.model.photo?
+            img
+        elsif type == 'photo'
+            image_generator(height: '350', width: '200')
+        elsif type == 'cover' 
+            image_generator(height: '600', width: '400')
+        end
+    end
 end
